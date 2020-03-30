d8f0fcd782417888e85f372c9b716aa6cb61a267
=rey-coyrehourcq <sebastien.rey-coyrehourcq@univ-rouen.fr>
2020-03-30 17:26:24 +0200

    add code for data caching, and modifying the docker compose to work with traeffik

--- covidapp/covidapp.py
+++ covidapp/covidapp.py
@@ -7,35 +7,43 @@ import time
 import pendulum
 import pandas as pd
 import shutil
-
-from dfply import *
-
-
-app = Flask(__name__)
-
-api = Api(app)
-
-def download_file(url,myauth):
-    local_filename = url.split('/')[-1]
-    with requests.get(url, auth=myauth, stream=True) as r:
-        with open(local_filename, 'wb') as f:
-            shutil.copyfileobj(r.raw, f)
-
-    return local_filename
-
 import requests
 from requests_ntlm import HttpNtlmAuth
+from dfply import *
+from pathlib import Path
+
+app = Flask(__name__)
+api = Api(app)
+
+def download_file(url,myauth, dataFolder):
+    local_filename = url.split('/')[-1]
+    p = dataFolder / local_filename
+    p.touch()
+    with requests.get(url, auth=myauth, stream=True) as r:
+        with p.open('wb') as f:
+            shutil.copyfileobj(r.raw, f)
+    return p
+
 
 class Ecdc(Resource):
     def get(self):
         today= pendulum.yesterday()
         todaystr=today.format("YYYY-MM-DD")
 
-        url = "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-" + todaystr +".xlsx"
-        print(url)
-        myauth = HttpNtlmAuth(":", ":")
+        root = Path.cwd() / 'covidapp'
 
-        r = download_file(url, myauth)
+        Path.mkdir(root / 'data', exist_ok=True)
+        dataFolder = root / 'data'
+        file = dataFolder / Path("COVID-19-geographic-disbtribution-worldwide-" + todaystr + ".xlsx")
+
+        if not file.exists():
+            url = "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-" + todaystr + ".xlsx"
+            myauth = HttpNtlmAuth(":", ":")
+            r = download_file(url, myauth, dataFolder)
+            print("file not exist")
+        else:
+            print("file exist")
+            r = file
         df = pd.read_excel(r)
 
         df_deaths = df >> group_by(X.geoId) >> arrange(X.dateRep, ascending=True) >> mutate(death_cum=cumsum(X.deaths))
@@ -43,7 +51,7 @@ class Ecdc(Resource):
 
         df_final = df_cases >> mask(X.geoId == "FR")
 
-        return {"response": df_final.to_json(orient='records')}
+        return {"response": df_final.to_json(orient='records', date_format='iso')}
 
 api.add_resource(Ecdc, '/covid19/ecdc/')
 
@@ -51,7 +59,7 @@ def print_date_time():
     print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=print_date_time, trigger="interval", seconds=5)
+scheduler.add_job(func=print_date_time, trigger="interval", day=1)
 
 scheduler.start()
 atexit.register(lambda: scheduler.shutdown())
--- test.py
+++ test.py
@@ -4,25 +4,42 @@ from requests_ntlm import HttpNtlmAuth
 import pendulum
 import pandas as pd
 import shutil
-today = pendulum.today()
+today = pendulum.yesterday()
 todaystr = today.format("YYYY-MM-DD")
 
-def download_file(url,myauth):
+
+def download_file(url,myauth, dataFolder):
     local_filename = url.split('/')[-1]
+
+    p = dataFolder / local_filename
+    p.touch()
+
     with requests.get(url, auth=myauth, stream=True) as r:
-        with open(local_filename, 'wb') as f:
+        with p.open('wb') as f:
             shutil.copyfileobj(r.raw, f)
 
-    return local_filename
+    return p
 
-
-url = "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-" + todaystr + ".xlsx"
-print(url)
-myauth = HttpNtlmAuth(":", ":")
 
 from dfply import *
 
-r = download_file(url, myauth)
+from pathlib import Path
+
+root =  Path.cwd() / 'covidapp'
+
+Path.mkdir(root/'data',exist_ok=True)
+dataFolder = root / 'data'
+file = dataFolder / Path("COVID-19-geographic-disbtribution-worldwide-" + todaystr + ".xlsx")
+
+if not file.exists():
+    url = "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-" + todaystr + ".xlsx"
+    myauth = HttpNtlmAuth(":", ":")
+    r = download_file(url, myauth, dataFolder)
+    print("file not exist")
+else:
+    print("file exist")
+    r = file
+
 df = pd.read_excel(r)
 
 df_deaths = df >> group_by(X.geoId) >> arrange(X.dateRep, ascending=True) >> mutate(death_cum= cumsum(X.deaths))
@@ -30,7 +47,7 @@ df_cases = df_deaths >> group_by(X.geoId) >> arrange(X.dateRep, ascending=True) 
 
 df_final = df_cases >>  mask(X.geoId == "FR")
 
-print()
+print(df_final.to_json(orient='records', date_format='iso'))
 
 #gb.apply(lambda _df: _df.sort_values('deaths', ascending=False)).apply(lambda _df: 'deaths'.cumsum())
 
