e9450cbf37c1087e8b1ab6cf26187fa211713a85
=rey-coyrehourcq <sebastien.rey-coyrehourcq@univ-rouen.fr>
2020-04-04 23:08:57 +0200

    add etalab api / scripts to process data with consolidation taken from github code used in dashboard https://dashboard.covid19.data.gouv.fr/ website.

--- covidapp/covidapp.py
+++ covidapp/covidapp.py
@@ -6,63 +6,147 @@ import atexit
 import time
 import pendulum
 import pandas as pd
+import json
 import shutil
 import requests
 from requests_ntlm import HttpNtlmAuth
 from dfply import *
 from pathlib import Path
 
+from etalab_processing import consolidate_data
+
 app = Flask(__name__)
 api = Api(app)
 
-def download_file(url,myauth, dataFolder):
-    local_filename = url.split('/')[-1]
-    p = dataFolder / local_filename
+
+def download_byte_file(url, filename, dataFolder, auth=None):
+    p = dataFolder / filename
     p.touch()
-    with requests.get(url, auth=myauth, stream=True) as r:
+    if auth != None:
+        with requests.get(url, auth=auth, stream=True) as r:
+            with p.open('wb') as f:
+                shutil.copyfileobj(r.raw, f)
+        return p
+    else:
+        with requests.get(url, stream=True) as r:
+            with p.open('wb') as f:
+                shutil.copyfileobj(r.raw, f)
+        return p
+
+def download_csv_file(url, filename, dataFolder):
+    p = dataFolder / filename
+    p.touch()
+    with requests.get(url) as r:
         with p.open('wb') as f:
-            shutil.copyfileobj(r.raw, f)
+            f.write(r.content)
     return p
+
+today = pendulum.today()
+todaystr = today.format("YYYY-MM-DD")
+
+
+def remoteFileExists(url):
+
+    r = requests.get(url, auth=HttpNtlmAuth(":", ":"), stream=True)
+    if r.status_code == 404:
+        return False
+    else:
+        return True
+
+def readyOrBackInTime(urlToTest):
+
+    # Data for today is not ready or not available, back in time or take in data already dl
+    if remoteFileExists(urlToTest) == False:
+        # Back In Time
+        f = pendulum.yesterday()
+        return f.format("YYYY-MM-DD")
+    else:
+        return todaystr
+
+class Etalab(Resource):
+
+    def __init__(self, **kwargs):
+        self.dataFolder = kwargs['dataFolder']
+        root = Path.cwd()
+
+        self.folder = Path(root / 'data' / self.dataFolder)
+        self.folder.mkdir(exist_ok=True)
+
+        self.col_names = {
+        "date" : "dateRep",
+        "cas_confirmes":"cases_cum",
+        "deces": "death_cum" ,
+        "reanimation":"ventilated" ,
+        "hospitalises":"hospitalized",
+        "gueris":"recover"
+        }
+
+    def get(self):
+
+        url = "https://raw.githubusercontent.com/opencovid19-fr/data/master/dist/chiffres-cles.csv"
+
+        fstr = readyOrBackInTime(url)
+
+        file = self.folder / Path("chiffres-cles-"+ fstr +".csv")
+
+        if not file.exists():
+            local_filename = "chiffres-cles-"+ fstr +".csv"
+            r = download_csv_file(url, local_filename, self.folder)
+        else:
+            r = file
+
+        pd.set_option('display.max_columns', 20)
+        pd.set_option('display.width', 500)
+
+        # FILTER maille_code = FRA
+        df = pd.read_csv(r)
+
+        SOURCE_PRIORITIES = {
+            1: 'ministere-sante',
+            2: 'sante-publique-france',
+            3: 'sante-publique-france-data',
+            4: 'opencovid19-fr'
+        }
+
+        df_final = consolidate_data(df,SOURCE_PRIORITIES)
+        #print(df_final.columns)
+        df_final.rename(columns=self.col_names, inplace=True)
+        df_final_json = df_final.to_json(orient='records', date_format='iso')
+
+        datajson = json.loads(df_final_json)
+
+        return {'data':datajson}
 
 
 class Ecdc(Resource):
-    def get(self):
-        today= pendulum.today()
-        todaystr=today.format("YYYY-MM-DD")
 
+    def __init__(self, **kwargs):
+        self.dataFolder = kwargs['dataFolder']
         root = Path.cwd()
 
-        Path.mkdir(root / 'data', exist_ok=True)
-        dataFolder = root / 'data'
+        self.folder = Path(root / 'data' / self.dataFolder)
+        self.folder.mkdir(exist_ok=True)
 
-        def remoteFileExists(d):
-            url = "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-" + d + ".xlsx"
-            myauth = HttpNtlmAuth(":", ":")
-            r = requests.get(url, auth=myauth, stream=True)
-            if r.status_code == 404:
-                return False
-            else:
-                return True
+        print("init with", self.folder)
 
-        # Data for today is not ready, back in time
-        if remoteFileExists(todaystr) == False:
-            # Back In Time
-            f = pendulum.yesterday()
-            fstr = f.format("YYYY-MM-DD")
-            print("back in time to ", )
-        else:
-            fstr = todaystr
+    def get(self):
 
-        file = dataFolder / Path("COVID-19-geographic-disbtribution-worldwide-" + fstr + ".xlsx")
+        url = "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-" + todaystr + ".xlsx"
+
+        fstr = readyOrBackInTime(url)
+
+        file = self.folder / Path("COVID-19-geographic-disbtribution-worldwide-" + fstr + ".xlsx")
 
         if not file.exists():
             url = "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-" + fstr + ".xlsx"
             myauth = HttpNtlmAuth(":", ":")
-            r = download_file(url, myauth, dataFolder)
+            local_filename = url.split('/')[-1]
+            r = download_byte_file(url, local_filename, self.folder, myauth)
             print("file not exist")
         else:
             print("file exist")
             r = file
+
         df = pd.read_excel(r)
 
         df_deaths = df >> group_by(X.geoId) >> arrange(X.dateRep, ascending=True) >> mutate(death_cum=cumsum(X.deaths))
@@ -72,13 +156,12 @@ class Ecdc(Resource):
 
         df_final_json = df_final.to_json(orient='records', date_format='iso')
 
-        import json
-
         datajson = json.loads(df_final_json)
 
         return {'data':datajson}
 
-api.add_resource(Ecdc, '/covid19/ecdc/')
+api.add_resource(Ecdc, '/covid19/ecdc/', resource_class_kwargs={'dataFolder': 'ecdc'} )
+api.add_resource(Etalab, '/covid19/etalab/', resource_class_kwargs={'dataFolder': 'etalab'})
 
 def print_date_time():
     print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
---
+++ covidapp/etalab_processing.py
@@ -0,0 +1,69 @@
+from dfply import *
+
+def consolidate_data(df, priorities):
+
+    ar = list(priorities.values())
+
+    def getPriority(id): return priorities[id]
+
+    @make_symbolic
+    def to_datetime(series):
+        return pd.to_datetime(series, format="%Y-%m-%d")
+
+    df_fra = df >> mask(X.maille_code == "FRA")
+    group_of_data = df_fra >> mask(X.source_type.isin(ar)) >> mutate(dt = to_datetime(X.date))
+    groupedData = group_of_data[group_of_data['dt'] > '2020-02-29'].groupby('source_type')
+
+    gDataStart = groupedData.get_group(getPriority(1))
+
+    import copy
+
+    def getValidDataFor(groupedData, date, column, acc, priority = 1 ) :
+        # get corresponding group, starting from 1
+        def gData(p) :
+            try:
+                fp = getPriority(p)
+                return groupedData.get_group(fp) >> mask(X.dt == date)
+            except KeyError:
+                return pd.DataFrame()
+
+        if priority > 4:
+            #Value not Found in other priorities, back to original data
+            acc.update({column : gData(1).get(column).values[0]})
+            return copy.deepcopy(acc)
+
+        pdata = gData(priority)
+
+        if pdata.empty:
+            # continue to test on the next priority sources if this key not exist
+            return getValidDataFor(groupedData, date, column, acc, priority + 1)
+        else:
+            # key exist but value is Nan, try with next priority sources
+            if pd.isna(pdata.get(column).values[0]) :
+                 return getValidDataFor(groupedData, date, column, acc, priority + 1 )
+            else:
+                if priority > 1 :
+                    print("At date ", date ," column ", column ," : ", gData(1).get(column).values[0]," replaced by ", gData(priority).get(column).values[0] , " at priority" , priority )
+                    acc.update({column : gData(priority)[column].values[0]})
+                    return copy.deepcopy(acc)
+                else :
+                    acc.update({column : gData(1).get(column).values[0]})
+                    return copy.deepcopy(acc)
+
+    def iterate_on_row(gdata, row):
+
+        columns = ["cas_confirmes", "deces", "reanimation", "hospitalises", "gueris"]
+        acc = {}
+        acc.update({"date" : row["dt"]})
+
+        for c in columns:
+            getValidDataFor(gdata, row["dt"], c, acc, 1)
+        return acc
+
+    rows_list = []
+
+    for index, row in gDataStart.iterrows():
+        rows_list.append(iterate_on_row(groupedData,row))
+
+    final_df = pd.DataFrame(rows_list)
+    return final_df
