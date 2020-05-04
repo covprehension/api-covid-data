add new computation, rolling windows for cumulated cases/deaths columns by type (ehpad, hospital). Fix metadata option in consequence.

covidapp/common/utils.py
@@ -60,6 +60,11 @@ def growth(series):
     return series.pct_change()
 
 @make_symbolic
+def rollbackcum(series):
+    return series.diff()
+
+
+@make_symbolic
 def normalized_growth(series,min,max):
     # reverse function nm to range min max
     # for index, value in series.iteritems():
@@ -91,26 +96,26 @@ def daysBeforeMultiply(x, **kwargs):
     dict_inverted = kwargs["dict"]
 
     # Skip the first line, returning Nan
-    if numericIndex == 0 or np.isnan(x['deaths_cum']):
+    if numericIndex == 0 or np.isnan(x['deaths_all_cum']):
         return x.name, np.NaN, np.NaN, np.NaN
 
     # If value_cum is the same than the previous row (nothing changed),
     # we need some tweaking (compute using the datebefore) to return same data
-    ilocvalue = kwargs["df"].iloc[[numericIndex - 1]]["deaths_cum"][0]
-    if x['deaths_cum'] == ilocvalue:
-        name = dict_inverted[x['deaths_cum']]
+    ilocvalue = kwargs["df"].iloc[[numericIndex - 1]]["deaths_all_cum"][0]
+    if x['deaths_all_cum'] == ilocvalue:
+        name = dict_inverted[x['deaths_all_cum']]
     else:
         name = x.name
 
     # Series to compare with actual row
-    series =  kwargs["deaths_cum"]
+    series =  kwargs["deaths_all_cum"]
     # Cut this series by taking in account only the days before actual date
     cutedSeries = series[series.index < name]
-    rowValueToCompare = float(x['deaths_cum'])
+    rowValueToCompare = float(x['deaths_all_cum'])
 
     # User query to filter rows
     # https://stackoverflow.com/questions/40171498/is-there-a-query-method-or-similar-for-pandas-series-pandas-series-query
-    result = cutedSeries.to_frame().query('deaths_cum > 0').query(f'({rowValueToCompare} / deaths_cum) >= 2.0')
+    result = cutedSeries.to_frame().query('deaths_all_cum > 0').query(f'({rowValueToCompare} / deaths_all_cum) >= 2.0')
 
     # If empty return Nan
     if result.empty:

covidapp/resources/ECDC.py
@@ -25,6 +25,8 @@ class Ecdc(Resource):
 
     def get(self):
 
+        # ECDC DATA ARE DAILY, we compute cumulated here.
+
         args = self.parser.parse_args()
         typeOfData = args['type']
         rolling = args['rolling']
@@ -58,11 +60,11 @@ class Ecdc(Resource):
 
         if (typeOfData == "cum"):
 
-            df_deaths = df_working >> arrange(X.dateRep, ascending=True) >> mutate(deaths_cum=cumsum(X.deaths))
+            df_deaths = df_working >> arrange(X.dateRep, ascending=True) >> mutate(deaths_all_cum=cumsum(X.deaths))
             df_cases = df_deaths >> arrange(X.dateRep, ascending=True) >> mutate(cases_cum=cumsum(X.cases))
-            dict_dup_deaths_cum = compute_dict(df_cases,"deaths_cum")
+            dict_dup_deaths_cum = compute_dict(df_cases,"deaths_all_cum")
 
-            df_deaths_growth = df_cases >> arrange(X.dateRep, ascending=True) >> mutate (growth_deaths_cum=growth(X.deaths_cum))
+            df_deaths_growth = df_cases >> arrange(X.dateRep, ascending=True) >> mutate (growth_deaths_cum=growth(X.deaths_all_cum))
             df_cases_growth = df_deaths_growth  >> arrange(X.dateRep, ascending=True) >> mutate(growth_cases_cum= growth(X.cases_cum))
 
             df_cases_growth = df_cases_growth.replace(np.inf, np.nan)
@@ -76,9 +78,9 @@ class Ecdc(Resource):
                 nm_growth_cases_cum=normalized_growth(X.growth_cases_cum, minnm, maxnm))
 
             df_cases_nmgrowth[["Xdatei", "XDatef", "XDelta", "XValue"]] = df_cases_nmgrowth.apply(daysBeforeMultiply, result_type="expand", dict=dict_dup_deaths_cum, df=df_cases_nmgrowth,
-                                                            deaths_cum=df_cases_nmgrowth['deaths_cum'], axis=1)
+                                                            deaths_all_cum=df_cases_nmgrowth['deaths_all_cum'], axis=1)
 
-            df_final = df_cases_nmgrowth >> select(X.dateRep, X.deaths_cum, X.cases_cum, X.growth_deaths_cum,
+            df_final = df_cases_nmgrowth >> select(X.dateRep, X.deaths_all_cum, X.cases_cum, X.growth_deaths_cum,
                                                    X.growth_cases_cum, X.nm_growth_deaths_cum, X.nm_growth_cases_cum, X.Xdatei, X.XDatef, X.XDelta, X.XValue)
         else:
 
@@ -92,9 +94,19 @@ class Ecdc(Resource):
             df_deaths_nmgrowth = df_cases_growth >> mutate(nm_growth_deaths_daily=normalized_growth(X.growth_deaths_daily, minnm,maxnm ))
             df_cases_nmgrowth = df_deaths_nmgrowth >> mutate(nm_growth_cases_daily=normalized_growth(X.growth_cases_daily, minnm,maxnm ))
 
-            df_rolling_mean_deaths = df_cases_nmgrowth >> mutate(rolling_deaths=rolling_mean(X.deaths, "{r}D".format(r=rolling), None))
+            df_rolling_mean_deaths = df_cases_nmgrowth >> mutate(rolling_deaths_daily=rolling_mean(X.deaths, "{r}D".format(r=rolling), None))
+            df_rolling_mean_cases = df_rolling_mean_deaths >> mutate(
+                rolling_cases_daily=rolling_mean(X.cases, "{r}D".format(r=rolling), None))
 
-            df_final = df_rolling_mean_deaths >> select(X.dateRep, X.deaths,X.cases,X.growth_deaths_daily,X.growth_cases_daily,X.nm_growth_deaths_daily,X.nm_growth_cases_daily,X.nm_growth_cases_daily, X.rolling_deaths)
+            df_final = df_rolling_mean_cases >> select(X.dateRep,
+                                                       X.deaths,
+                                                       X.cases,
+                                                       X.growth_deaths_daily,
+                                                       X.growth_cases_daily,
+                                                       X.nm_growth_deaths_daily,
+                                                       X.nm_growth_cases_daily,
+                                                       X.rolling_cases_daily,
+                                                       X.rolling_deaths_daily)
             df_final.rename(columns=self.dayly_col_names, inplace=True)
 
         #df_final.to_csv('outxx.csv')